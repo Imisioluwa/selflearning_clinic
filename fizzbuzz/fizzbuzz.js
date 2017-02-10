86aa4bf2a03d7fe3f7e01ff88bddc9b053a0dd7b
Imisioluwa <imizezek@yahoo.com>
2017-02-10 00:29:17 -0800

    Completed number of test cases required and the use of camelCase styling with two space indentation

--- fizzbuzz/fizzbuzz.js
+++ fizzbuzz/fizzbuzz.js
@@ -1,6 +1,6 @@
 // Function that determines if a number is divisible by 3 and or 5
 function fizzbuzz(numB){
-  var fizz_buzz = "";
+  var fizzBuzz = "";
   if (numB === 0){
   	return 0;
   }
@@ -10,17 +10,17 @@ function fizzbuzz(numB){
   // Number divisible by three remainder zero
   if (numB % 3 === 0){      
   // Returns Fizz
-    fizz_buzz += "Fizz"; 
+    fizzBuzz += "Fizz"; 
   }
   // Number divisible by five remainder zero
   if (numB % 5 === 0){ 
   //  Returns Buzz     
-    fizz_buzz += "Buzz"; 
+    fizzBuzz += "Buzz"; 
   }
   //Number divisible by 15 will also be divisible by three and five with remainder zero
-  if (fizz_buzz){  
+  if (fizzBuzz){  
   // Return fizz_buzz        
-    return fizz_buzz;    
+    return fizzBuzz;    
   }
   else{
   //Return number if it is indivisible by three and five
