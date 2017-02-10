60062731f254b74b98952280a175f8a4d07dbedd
Imisioluwa <imizezek@yahoo.com>
2017-02-10 00:08:30 -0800

    Implemented camelCase style on the parameter name

--- aritGeo/arithGeo.js
+++ aritGeo/arithGeo.js
@@ -1,25 +1,39 @@
-function aritGeo(numarray){   // Function aritGeo holding a single parameter which is an array of numbers
-	if (numarray == 0){   // If array is empty then the function returns zero
-		return 0;
-	} 
-	var ratio = numarray[1]/numarray[0];//second element in the array divided by the first array is equal to ratio
-	var difference = numarray[1] - numarray[0]; //first element in the array substracted from second element in the array
-	for (var index = 1; index < numarray.length - 1; index++){  // Looping through the array 
-		if (numarray[index + 1]/numarray[index] != ratio){ // Break the if condition when the third element in the array divided 
-			break;                                         // by the second element is not equal to "ratio"
-		}
-		if (index == numarray.length - 2){                 // Compare index to the last element of the array 
-			return "Geometric";
-		}
+// Function aritGeo holding a single parameter which is an array of numbers
+function aritGeo(numArray){  
+// If array is empty then the function returns zero 
+  if (numArray.length === 0){   
+    return 0;
+  } 
+  //second element in the array divided by the first array is equal to ratio
+  var ratio = numArray[1]/numArray[0];
+  //first element in the array substracted from second element in the array
+  var difference = numArray[1] - numArray[0];
+  // Looping through the array 
+  for (var index = 1; index < numArray.length - 1; index++){
+  // Break the if condition when the third element in the array divided   
+    if (numArray[index + 1]/numArray[index] != ratio){  
+	  // by the second element is not equal to "ratio"
+	  break;                                         
+	}
+	// Compare index to the last element of the array
+	if (index === numArray.length - 2){                  
+	  return "Geometric";
+	}
+  }
+  // Looping through the array
+  for (var index = 1; index < numArray.length - 1; index++){ 
+  	// Break the if condition when the third element in the array divided
+    if (numArray[index + 1] - numArray[index] != difference){ 
+      // by the second element is not equal to "difference"
+      break;                                                
     }
-    for (var index = 1; index < numarray.length - 1; index++){ // Looping through the array
-    	if (numarray[index + 1] - numarray[index] != difference){ // Break the if condition when the third element in the array divided
-    		break;                                                // by the second element is not equal to "difference"
-        }
-        if (index == numarray.length - 2){                        // Compare index to the last element of the array
-        	return "Arithmetic";
-        }
+    // Compare index to the last element of the array
+    if (index === numArray.length - 2){                        
+      return "Arithmetic";
     }
-    return -1;                                      // The function returns -1 if the array is neither Arithmetic or Geometric
+  }
+  // The function returns -1 if the array is neither Arithmetic or Geometric
+  return -1;
 }
+
 module.exports = aritGeo;
